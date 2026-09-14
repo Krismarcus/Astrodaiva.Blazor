4dd9b6e3ea4ff445b0f7c7c04968909b0791b79e
krismarcus <krisktupats@gmail.com>
2026-09-14 10:21:14 +0200

    Reserve day-card frame for today and deepen eclipse shading

--- wwwroot/service-worker.js
+++ wwwroot/service-worker.js
@@ -1,9 +1,9 @@
-const CACHE_NAME = "astrodaiva-shell-v20260914-first-moon-day-v1";
+const CACHE_NAME = "astrodaiva-shell-v20260914-today-frame-v1";
 
 const APP_SHELL = [
   "./",
   "./index.html",
-  "./css/app.css?v=20260914-first-moon-day-v1",
+  "./css/app.css?v=20260914-today-frame-v1",
   "./manifest.webmanifest",
   "./img/astrodaiva-logo.png",
   "./img/pwa/apple-touch-icon.png",
